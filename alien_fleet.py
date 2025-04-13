30db92e3a5aba382831cadd85d877100a85a8918
liondog2 <tylerthompson514@gmail.com>
2025-04-13 16:22:29 -0400

    Part 2- Modified the battalion to also create all the rows. Changed background size. Made changes to gameplay settings.

--- alien_fleet.py
+++ alien_fleet.py
@@ -16,29 +16,48 @@ class AlienFleet:
         self.create_fleet()
 
     def create_fleet(self) -> None:
+        # Alien size
+        alien_w = self.settings.alien_w
         alien_h = self.settings.alien_h
+        # Screen size
+        screen_w = self.settings.screen_w
         screen_h = self.settings.screen_h
 
-        fleet_h = self.calculate_fleet_size(alien_h, screen_h)
+        fleet_w, fleet_h = self.calculate_fleet_size(alien_w, screen_w, alien_h, screen_h)
+        print()
+        print(fleet_w, fleet_h)
         
-        # half_screen = self.settings.screen_h
+        half_screen = self.settings.screen_w//2
+        fleet_horizontal_space = fleet_w * alien_w
         fleet_vertical_space = fleet_h * alien_h
+
+        x_offset = int((half_screen-fleet_horizontal_space)//2)
         y_offset = int((screen_h-fleet_vertical_space)//2)
+        for col in range(fleet_w):
+            for row in range(fleet_h):
+                current_x = alien_w * col + x_offset
+                current_y = alien_h * row + y_offset
+                if row % 2 == 0 or col % 2 == 0:
+                    continue
+                self._create_alien(current_x, current_y)
 
-        for row in range(fleet_h):
-            current_y = alien_h * row + y_offset
-            if row % 2 == 0:
-                continue
-            self._create_alien(10, current_y)
-
-    def calculate_fleet_size(self, alien_h, screen_h) -> None:
+    def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h) -> tuple:
+        print(screen_w, alien_w)
+        fleet_w = ((screen_w/2)//alien_w)
         fleet_h = (screen_h//alien_h)
+        
+        # Width
+        if fleet_w % 2 == 0:
+            fleet_w -= 1
+        else:
+            fleet_w -= 2
+        
+        # Height
         if fleet_h % 2 == 0:
             fleet_h -= 1
         else:
             fleet_h -= 2
-        
-        return fleet_h
+        return int(fleet_w), int(fleet_h)
     
     def _create_alien(self, current_x: int, current_y: int) -> None:
         new_alien = Alien(self, current_x, current_y)
--- settings.py
+++ settings.py
@@ -3,15 +3,15 @@ class Settings:
 
     def __init__(self):
         self.name: str = 'Alien Invasion'
-        self.screen_w = 800
-        self.screen_h = 1200
+        self.screen_w = 1820
+        self.screen_h = 1048
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'background.png'
 
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'tank_player.png'
         self.ship_w = 128
         self.ship_h = 106
-        self.ship_speed = 5
+        self.ship_speed = 5.5
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'bullet.png'
         # For later
@@ -21,10 +21,10 @@ class Settings:
         # }
 
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'fireProjectile1.ogg'
-        self.bullet_speed = 7
+        self.bullet_speed = 8.5
         self.bullet_w = 32
         self.bullet_h = 16
-        self.bullet_amount = 5
+        self.bullet_amount = 6
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'tank_enemy.png'
         self.alien_w = 128
