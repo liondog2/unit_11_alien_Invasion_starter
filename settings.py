eeff98c0af4d4341129f76b746cd4591da916c0c
liondog2 <tylerthompson514@gmail.com>
2025-04-19 00:30:07 -0400

    Changed font

--- settings.py
+++ settings.py
@@ -39,7 +39,7 @@ class Settings:
         self.text_color = (255, 255, 255)
         self.button_font_size = 48
         self.HUD_font_size = 20
-        self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf'
+        self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'KeaniaOne' / 'KeaniaOne-Regular.ttf'
 
     def initialize_dynamic_settings(self) -> None:
         self.ship_speed = 5.5
